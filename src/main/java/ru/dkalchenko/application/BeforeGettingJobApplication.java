a09a6441f2c9457335242b02a437e7bca305b231
denfort50 <denfort50@yandex.ru>
2023-05-18 12:45:01 +0300

    Configuration added

---
+++ src/main/java/ru/dkalchenko/application/BeforeGettingJobApplication.java
@@ -0,0 +1,13 @@
+package ru.dkalchenko.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeforeGettingJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeforeGettingJobApplication.class, args);
+    }
+
+}
